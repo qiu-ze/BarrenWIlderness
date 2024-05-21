86db1ed781c512d50770375f37d35086f3d64c3b
M1nakami <a378499394@163.com>
2024-05-21 23:39:22 +0900

    first commit

---
+++ src/main/java/net/qiuze/neoforgebw/BarrenWilderness.java
@@ -0,0 +1,36 @@
+package net.qiuze.neoforgebw;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.qiuze.neoforgebw.item.ModItems;
+import org.slf4j.Logger;
+
+@Mod(BarrenWilderness.MODID)
+public class BarrenWilderness
+{
+    public static final String MODID = "neoforgebw";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public BarrenWilderness(IEventBus modEventBus, ModContainer modContainer)
+    {
+        modEventBus.addListener(this::commonSetup);
+        ModItems.register(modEventBus);
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::commonSetup);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+}
