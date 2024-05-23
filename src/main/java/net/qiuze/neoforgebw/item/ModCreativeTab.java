23e0ce9fb2b0fd34b9a20f7b18887274dff79d55
M1nakami <a378499394@163.com>
2024-05-23 11:25:22 +0900

    creativetab

--- src/main/java/net/qiuze/neoforgebw/item/ModCreativeTab.java
+++ src/main/java/net/qiuze/neoforgebw/item/ModCreativeTab.java
@@ -1,2 +1,29 @@
-package net.qiuze.neoforgebw.item;public class ModCreativeTab {
+package net.qiuze.neoforgebw.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.qiuze.neoforgebw.BarrenWilderness;
+
+import java.util.function.Supplier;
+
+public class ModCreativeTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BarrenWilderness.MODID);
+
+    public static final String BARREN_WILDERNESS_TAB_STRING = "creativetab.barren_tab";
+
+    public static final Supplier<CreativeModeTab> BARREN_TAB = CREATIVE_MODE_TABS.register("barren_tab",() -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .title(Component.translatable(BARREN_WILDERNESS_TAB_STRING))
+            .icon(() ->ModItems.STONECOIN.get().getDefaultInstance())
+            .displayItems((pParameters, pOutput) -> {
+                pOutput.accept(ModItems.STONECOIN.get());
+            })
+            .build());
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
