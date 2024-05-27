7b05a3f84e2a12157ea9b768144ace7be2b15bdf
M1nakami <a378499394@163.com>
2024-05-27 20:08:59 +0900

    change use to useItemOn,and InteractionResult to ItemInteractionResult can light block.space should not add to json

--- src/main/java/net/qiuze/neoforgebw/block/custom/BloodBlock.java
+++ src/main/java/net/qiuze/neoforgebw/block/custom/BloodBlock.java
@@ -3,7 +3,9 @@ package net.qiuze.neoforgebw.block.custom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -21,13 +23,13 @@ public class BloodBlock extends Block {
 
     }
 
-    public InteractionResult useWithoutItem(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+    public ItemInteractionResult useItemOn(ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
 
     if (!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
         pLevel.setBlock(pPos, pState.cycle(FIRE), 3);
     }
 
-    return super.useWithoutItem(pState,pLevel,pPos,pPlayer,pHit);
+    return super.useItemOn(pStack,pState,pLevel,pPos,pPlayer,pHand,pHit);
 
     }
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
