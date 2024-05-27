c61d0a3c3cde3c147ba833cedd735b9d478a47be
M1nakami <a378499394@163.com>
2024-05-27 20:11:21 +0900

    change use to useItemOn,and InteractionResult to ItemInteractionResult can light block.space should not add to json

--- src/main/java/net/qiuze/neoforgebw/block/custom/BloodBlock.java
+++ src/main/java/net/qiuze/neoforgebw/block/custom/BloodBlock.java
@@ -2,17 +2,16 @@ package net.qiuze.neoforgebw.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 
 public class BloodBlock extends Block {
     public static final BooleanProperty FIRE = BooleanProperty.create("fire");
@@ -23,7 +22,7 @@ public class BloodBlock extends Block {
 
     }
 
-    public ItemInteractionResult useItemOn(ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+    public @NotNull ItemInteractionResult useItemOn(@NotNull ItemStack pStack, @NotNull BlockState pState, Level pLevel, @NotNull BlockPos pPos, @NotNull Player pPlayer, @NotNull InteractionHand pHand, @NotNull BlockHitResult pHit) {
 
     if (!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
         pLevel.setBlock(pPos, pState.cycle(FIRE), 3);
