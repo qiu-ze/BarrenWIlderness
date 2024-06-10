edb3f14a6b4f529bc43b8d204a8ff6eb7614add0
M1nakami <a378499394@163.com>
2024-06-10 22:34:10 +0800

    Correct extends

--- src/main/java/net/qiuze/neoforgebw/block/custom/BingBlock.java
+++ src/main/java/net/qiuze/neoforgebw/block/custom/BingBlock.java
@@ -1,8 +1,9 @@
 package net.qiuze.neoforgebw.block.custom;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
-public class BingBlock extends BloodBlock{
+public class BingBlock extends Block {
     public BingBlock(){
         super(Properties.ofFullCopy(Blocks.GLASS).strength(4).noOcclusion());
     }
